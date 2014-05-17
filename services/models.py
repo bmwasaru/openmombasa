b37218fb406f0bd444a03108a9efcae8b0e25668
Britone Mwasaru <bmwasaru@gmail.com>
2014-05-17 21:26:53 +0300

    Added class Meta

--- services/models.py
+++ services/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 
 class Service(models.Model):
+    """
+    Model to store a service and it's price
+    """
     name = models.CharField(max_length=100)
     price = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ('name',)
 
     def str(self):
         return self.name, self.price
